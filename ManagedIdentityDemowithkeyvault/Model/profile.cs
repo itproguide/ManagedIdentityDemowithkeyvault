ca4f1ae8fae9c0df17ec7dcc4d1105c09096f4a6
Mohammed Niyas <43110927+itproguide@users.noreply.github.com>
2023-02-06 14:43:24 +0400

    Update profile.cs
    fixed error "Microsoft.Data.SqlClient.SqlException (0x80131904): Operand type clash: nvarchar(4000) encrypted with (encryption_type = 'DETERMINISTIC', encryption_algorithm_name = 'AEAD_AES_256_CBC_HMAC_SHA_256', column_encryption_key_name = 'CEK_Auto1', column_encryption_key_database_name = 'ectesting') is incompatible with nvarchar(50) encrypted with (encryption_type = 'DETERMINISTIC', encryption_algorithm_name = 'AEAD_AES_256_CBC_HMAC_SHA_256', column_encryption_key_name = 'CEK_Auto1', column_encryption_key_database_name = 'ectesting')"
    
    added data annotations to the model

--- ManagedIdentityDemowithkeyvault/Model/profile.cs
+++ ManagedIdentityDemowithkeyvault/Model/profile.cs
@@ -2,9 +2,12 @@
 {
     public class profile
     {
+        [Key]
         public int id { get; set; }
-        public string name { get; set; }
-        public string job { get; set; }
-        public string mobile { get; set; }
+        public string? name { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string? job { get; set; }
+        [Column(TypeName = "nchar(10)")]
+        public string? mobile { get; set; }
     }
 }
